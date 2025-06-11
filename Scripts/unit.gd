1361a78e41e12056f02ad23abd61115ee6f0e6c3
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-06-11 15:46:33 +0200

    add unit movement and selection

--- Scripts/unit.gd
+++ Scripts/unit.gd
@@ -1,13 +1,36 @@
 extends CharacterBody2D
 
 @export var selected = false
+
 @onready var box = get_node("Box")
+@onready var target = position
+@onready var animation = get_node("AnimationPlayer")
+
+var follow_cursor = false
+var speed = 50
 	
 func _ready():
 	set_selected(selected)
 	
 func set_selected(value):
+	selected = value
 	box.visible = value
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+func _input(event):
+	if event.is_action_pressed("RightClick"):
+		follow_cursor = true
+	if event.is_action_released("RightClick"):
+		follow_cursor = false
+
+func _physics_process(delta):
+	if follow_cursor == true:
+		if selected:
+			target = get_global_mouse_position()
+			animation.play("WalkDown")
+			
+	velocity = position.direction_to(target) * speed
+	
+	if position.distance_to(target) > 15:
+		move_and_slide()
+	else:
+		animation.stop()
