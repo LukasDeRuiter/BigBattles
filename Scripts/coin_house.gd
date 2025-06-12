b57e1840c186b0138ad6a4a2947296129dba3a2b
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-06-12 16:47:29 +0200

    add coin building

---
+++ Scripts/coin_house.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+var totalTime = 50
+var currentTime
+
+@onready var bar = $ProgressBar
+@onready var timer = $Timer
+
+func _ready():
+	currentTime = totalTime
+	bar.max_value = totalTime
+	timer.start()
+
+func _process(delta):
+	if currentTime <= 0:
+		coinsCollected()
+
+func _on_timer_timeout():
+	currentTime -= 1
+	var tween = get_tree().create_tween()
+	tween.tween_property(bar, "value", currentTime, 0.1).set_trans(Tween.TRANS_LINEAR)
+	
+func coinsCollected():
+	Game.coin += 10
