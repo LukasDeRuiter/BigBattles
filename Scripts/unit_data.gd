4d64b003fca67b8d2ad7b6fdf25631421f3774b4
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-06-16 16:00:17 +0200

    finish barbarian axeman scene

---
+++ Scripts/unit_data.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name UnitData
+
+@export var name: String
+@export var unit_scene: PackedScene
+@export var icon: Texture2D
+@export var cost: int = 0
+@export var train_time: float = 1.5
