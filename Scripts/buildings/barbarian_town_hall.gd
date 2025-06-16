ebedced6283d3c3767766dad188b3786e79aefa1
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-06-16 23:22:34 +0200

    add all logic for town hall placement

--- Scripts/buildings/barbarian_town_hall.gd
+++ Scripts/buildings/barbarian_town_hall.gd
@@ -11,15 +11,6 @@ var trainable_units = []
 
 func _process(delta: float):
 	select.visible = selected
-	
-	if training_queue.size() > 0:
-		current_train_time -= delta
-		
-		if current_train_time <= 0:
-			var unit_to_train = training_queue.pop_front()
-			spawn_unit(unit_to_train)
-			
-			current_train_time = unit_to_train.training_time if unit_to_train.has_method("training_time") else 3.0
 
 func selectBuilding():
 	selected = true
@@ -42,19 +33,4 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	mouseEntered = false
-
-func queue_train_unit(unit: UnitData):
-	training_queue.append(unit)
-	
-func spawn_unit(unit: UnitData):
-	var instance = unit.unit_scene.instantiate()
-	var unitPath = get_tree().get_root().get_node("World/Units")
-	
-	unitPath.add_child(instance)
-	
-	instance.name = unit.name + "_" + str(instance.get_instance_id())
-	instance.position = global_position + Vector2(0, 32)
-	instance.target = instance.position
-	
-	unitPath.add_child(instance)
 	
