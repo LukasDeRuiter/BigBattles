b239282545d4181a7a546284b2a3d3902e7a5632
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-07-01 14:58:43 +0200

    add selection details scene for building and single unit select

--- Scripts/ui/selection_panel.gd
+++ Scripts/ui/selection_panel.gd
@@ -1,22 +1,32 @@
 extends Control
 
-@onready var container  = $FlowContainer
+@onready var container = $FlowContainer
+@onready var unitDetailsContainer = $SelectionUnitDetails
 @onready var cardTemplate = preload("res://Scenes/selection_card.tscn")
+@onready var detailsContainer = preload("res://Scenes/selection_details.tscn")
 
 func show_units(units):
 	clear()
 	
-	for unit in units:
-		var card = cardTemplate.instantiate()
-		container.add_child(card)
-		card.setup(unit)
+	if units.size() == 1:
+		var detailsContainerObject = detailsContainer.instantiate()
+		container.add_child(detailsContainerObject)
+		detailsContainerObject.setup(units.front())
+		detailsContainerObject.visible = true
+		
+	else:
+		for unit in units:
+			var card = cardTemplate.instantiate()
+			container.add_child(card)
+			card.setup(unit)
 
 func show_building(building):
 	clear()
 	
-	var card = cardTemplate.instantiate()
-	container.add_child(card)
-	card.setup(building)
+	var detailsContainerObject = detailsContainer.instantiate()
+	container.add_child(detailsContainerObject)
+	detailsContainerObject.setup(building)
+	detailsContainerObject.visible = true
 	
 func clear():
 	for child in container.get_children():
