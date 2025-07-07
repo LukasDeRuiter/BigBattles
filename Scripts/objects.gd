e5882687048deeea98b17f132e1207b36f88fb51
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-07-07 14:10:54 +0200

    add generation logic from objects to world

--- Scripts/objects.gd
+++ Scripts/objects.gd
@@ -15,53 +15,7 @@ const TileTypes = preload("res://Scripts/enums/tile_types.gd")
 enum { OBSTACLE, COLLECTABLE, RESOURCE }
 
 func _ready():
-	tilemap = get_tree().get_root().get_node("World/TileMapLayer")
-	var tilemap_bounds = tilemap.get_used_rect()
-	
-	var world_rect = Rect2(
-	tilemap_bounds.position * tilemap.tile_set.tile_size,
-	tilemap_bounds.size * tilemap.tile_set.tile_size
-)
-	
-	var noise := FastNoiseLite.new()
-	noise.noise_type = FastNoiseLite.TYPE_PERLIN
-	noise.frequency = 0.05
-	noise.seed = randi() 
-	
-	for x in range(grid_size.x):
-		for y in range(grid_size.y):
-				
-			var raw_value = noise.get_noise_2d(x, y)
-			var scaled_value = int(((raw_value + 1.0) / 2.0) * 1000)
-			var tile_type = "GRASS"
-	
-			if scaled_value > 500:
-				tile_type = "GRASS"
-			elif scaled_value > 400:
-				tile_type = "DIRT"
-			else:
-				tile_type = "WATER"
-			
-			var atlas_coords = TileTypes.ATLAS_COORDS[tile_type]
-			tilemap.set_cell(Vector2i(x, y), 0, atlas_coords)
-			if tile_type == "GRASS":
-				if scaled_value > 600:
-					var tree_instance = tree.instantiate()
-					tree_instance.position = Vector2(x, y) * tile_size + Vector2(tile_size, tile_size) / 2
-					add_child(tree_instance)
-					tree_instance.add_to_group("objects", true)
-					var grid_pos = grid_node.world_to_grid(tree_instance.position)
-					grid_node.register_tree(grid_pos, tree_instance)
-					grid_node.block_tile(grid_pos)
-					grid_node.block_tile_navigation(grid_pos)
-				elif randf() < 0.01:
-					var gold_instance = gold_ore.instantiate()
-					gold_instance.position = Vector2(x, y) * tile_size + Vector2(tile_size, tile_size) / 2
-					add_child(gold_instance)
-					var gold_grid_pos = grid_node.world_to_grid(gold_instance.position)
-					grid_node.register_gold_ore(gold_grid_pos, gold_instance)
-					grid_node.block_tile(gold_grid_pos)
-					grid_node.block_tile_navigation(gold_grid_pos)
+	pass
 	
 func _input(event):
 	pass
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -3,9 +3,18 @@ extends Node2D
 var units =  []
 var buildings = []
 
+var tile_size = 16
+var grid_size = Vector2(160, 160)
+
 @onready var command_manager = $CommandManager
 @onready var pause_menu = $PauseMenu
 @onready var grid = $Grid
+@onready var tilemap = $TileMapLayer
+@onready var objects = $Objects
+@onready var tree = preload("res://Scenes/objects/tree.tscn")
+@onready var gold_ore = preload("res://Scenes/objects/gold_ore.tscn")
+
+const TileTypes = preload("res://Scripts/enums/tile_types.gd")
 
 ## Survival Mode
 @onready var wave_timer = Timer.new()
@@ -16,6 +25,56 @@ func _ready():
 	
 	if Game.current_mode == "SURVIVAL":
 		setup_survival_mode()
+	
+	generate_world()
+	
+func generate_world():
+	var tilemap_bounds = tilemap.get_used_rect()
+	
+	var world_rect = Rect2(
+	tilemap_bounds.position * tilemap.tile_set.tile_size,
+	tilemap_bounds.size * tilemap.tile_set.tile_size
+)
+	
+	var noise := FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	noise.frequency = 0.05
+	noise.seed = randi() 
+	
+	for x in range(grid_size.x):
+		for y in range(grid_size.y):
+				
+			var raw_value = noise.get_noise_2d(x, y)
+			var scaled_value = int(((raw_value + 1.0) / 2.0) * 1000)
+			var tile_type = "GRASS"
+	
+			if scaled_value > 500:
+				tile_type = "GRASS"
+			elif scaled_value > 400:
+				tile_type = "DIRT"
+			else:
+				tile_type = "WATER"
+			
+			var atlas_coords = TileTypes.ATLAS_COORDS[tile_type]
+			tilemap.set_cell(Vector2i(x, y), 0, atlas_coords)
+			if tile_type == "GRASS":
+				if scaled_value > 600:
+					var tree_instance = tree.instantiate()
+					tree_instance.position = Vector2(x, y) * tile_size + Vector2(tile_size, tile_size) / 2
+					objects.add_child(tree_instance)
+					tree_instance.add_to_group("objects", true)
+					var grid_pos = grid.world_to_grid(tree_instance.position)
+					grid.register_tree(grid_pos, tree_instance)
+					grid.block_tile(grid_pos)
+					grid.block_tile_navigation(grid_pos)
+				elif randf() < 0.01:
+					var gold_instance = gold_ore.instantiate()
+					gold_instance.position = Vector2(x, y) * tile_size + Vector2(tile_size, tile_size) / 2
+					objects.add_child(gold_instance)
+					var gold_grid_pos = grid.world_to_grid(gold_instance.position)
+					grid.register_gold_ore(gold_grid_pos, gold_instance)
+					grid.block_tile(gold_grid_pos)
+					grid.block_tile_navigation(gold_grid_pos)
 	
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
